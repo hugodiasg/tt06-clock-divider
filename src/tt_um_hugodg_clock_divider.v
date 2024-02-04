f02345c7dda10babdae23de558f668e972c82c43
Hugo Dias Giló <80465879+hugodiasg@users.noreply.github.com>
2024-02-04 19:30:33 -0300

    Update tt_um_hugodg_clock_divider.v

--- src/tt_um_hugodg_clock_divider.v
+++ src/tt_um_hugodg_clock_divider.v
@@ -11,7 +11,7 @@ module tt_um_hugodg_clock_divider (
 
 // unused ports
 assign uio_out[7] = 0;  /* uio[7]: unused port grounded */
-assign uio_oe[7] = 8'b0;  /* uio[7]: unused port set */
+assign uio_oe[7:0] = 8'b0;  /* uio[7]: unused port set */
 assign uo_out[7:1] = 0;  /* uo[7]: unused port grounded */
     
 clock_divider clock_divider_inst(
