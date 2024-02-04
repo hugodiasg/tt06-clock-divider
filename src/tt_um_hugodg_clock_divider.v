89636021428208e403cb5ab5cf69c6635cf6816f
Hugo Dias Giló <80465879+hugodiasg@users.noreply.github.com>
2024-02-04 16:31:26 -0300

    Update and rename tt_um_clock_divider.v to tt_um_hugodg_clock_divider.v

--- src/tt_um_hugodg_clock_divider.v
+++ src/tt_um_hugodg_clock_divider.v
@@ -1,4 +1,4 @@
-module tt_um_clock_divider (
+module tt_um_hugodg_clock_divider (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
