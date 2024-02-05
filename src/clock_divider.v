2703d6d321a4a73fbe864b72865fd971040c077c
Hugo Dias Giló <80465879+hugodiasg@users.noreply.github.com>
2024-02-05 12:06:17 -0300

    Update clock_divider.v

--- src/clock_divider.v
+++ src/clock_divider.v
@@ -8,15 +8,15 @@ module clock_divider
   output clk_out 
 );
   integer count ; // counter
-  parameter CONST = 2 ;// 200000 ; // CONSTANT to multiply the 'scale'
-  reg  [WIDTH-1:0] scale_reg ; // register to save the scale_reg when reset is triggered
-  reg  [15:0] true_scale ; // the 'true' scale defined as 'scale_reg * CONST'
+  integer CONST = 2 ;// 200000 ; // CONSTANT to multiply the 'scale'
+  //reg  [WIDTH-1:0] scale_reg ; // register to save the scale_reg when reset is triggered
+  reg  [31:0] true_scale ; // the 'true' scale defined as 'scale_reg * CONST'
   reg signal_clk_out ; // register to save the clk_out and send it to this port
 
   initial begin
       signal_clk_out = 0 ;
       count = 0 ;   
-      scale_reg [WIDTH-1:0] = 0 ;
+      //scale_reg [WIDTH-1:0] = scale ;
       signal_clk_out = 0 ;
   end
 
@@ -24,7 +24,7 @@ module clock_divider
     if (!nrst) begin
       signal_clk_out <= 0 ;
       count <= 0 ;
-      scale_reg <=  scale; // set the scale to a internal register scale_reg when update_scale is triggered
+  //    scale_reg <=  scale; // set the scale to a internal register scale_reg when update_scale is triggered
       true_scale <= scale*CONST ; // update the true scale
     end
     else begin
@@ -42,3 +42,4 @@ module clock_divider
   assign clk_out = (true_scale == 0) ? clk_in : signal_clk_out ; // if the true_scale is zero, the clk_in is buffered to the output
 
 endmodule
+
